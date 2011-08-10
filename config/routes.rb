6160aa3219fd58e5185f036eb39466b6383a314f
Dan Cunningham <dan@wellbeinginthecity.me>
2011-08-10 16:45:50 +0100

    Routes and Integration testing set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact',   :to => 'pages#contact'
+  match '/about',     :to => 'pages#about'
+  match '/help',      :to => 'pages#help'
+  match '/signin',    :to => 'pages#about'
+  match '/signup',    :to => 'pages#about'
 
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
+  root :to => "pages#home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +56,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  # root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
