2c0c5496fd8dfaca504d160e07054ed20610e307
Dan Cunningham <dan@wellbeinginthecity.me>
2011-08-10 15:37:58 +0100

    Added help page and Blueprint CSS framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns a title on a per-page basis
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base.title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
